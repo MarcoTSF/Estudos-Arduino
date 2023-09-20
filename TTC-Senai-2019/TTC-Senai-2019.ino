1ebeae4172409c1cffb1661bbb93c43079e71a43
Marco Tulio Salvador Filho <mtsf.salvador@gmail.com>
2023-09-20 19:58:46 -0300

    feat: remove rf module and add more led's

--- TTC-Senai-2019/TTC-Senai-2019.ino
+++ TTC-Senai-2019/TTC-Senai-2019.ino
@@ -4,26 +4,35 @@ Ultrasonic ultrasonic(7);
 
 long distancia;
 
+int motorPin = A5;
+
 void setup() {
   // Função que roda apenas uma vez, quando ligada ou resetada
   Serial.begin(9600); // Habilita a comunicação serial
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(motorPin, OUTPUT);
 }
 
 void loop() {
   // Função que roda infinitamente depois que a placa é ligada
-  distancia = ultrasonic.MeasureInCentimeters();
+  distancia = ultrasonic.read(); // Correção aqui, remova "CM"
   Serial.print(distancia); // 0~400cm
   Serial.println("cm");
 
   if (distancia < 100) {
-    analogWrite(10, 200);
+    analogWrite(motorPin, 250);
     digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
   }
   else {
-    analogWrite(10, 10);
+    analogWrite(motorPin, 0);
     digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
   }
 
   delay(100);
