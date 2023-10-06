7eba51d140bfb7becef636162c83d129389fc49d
Marco Tulio Salvador Filho <mtsf.salvador@gmail.com>
2023-10-06 20:17:59 -0300

    feat: Update script to use a vibration motor

--- Comunicação-MódulosRF/Receptor/Receptor.ino
+++ Comunicação-MódulosRF/Receptor/Receptor.ino
@@ -3,34 +3,35 @@
 
 RH_ASK driver; //CRIA O DRIVER PARA COMUNICAÇÃO
 
-const int pinoLed = 4; //PINO DIGITAL UTILIZADO PELO LED
+int motorPin = A5; // Pino analógico A5
+
+const int pinoLed = 8; //PINO DIGITAL UTILIZADO PELO LED
 String str = ""; //VARIÁVEL DO TIPO STRING
-int statusLed = 0; //VARIÁVEL DO TIPO INT (CONTROLA O ESTADO ATUAL DO LED)
 
 void setup(){
-    driver.init(); //INICIALIZA A COMUNICAÇÃO RF DO DRIVER
-    pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
-    digitalWrite(pinoLed, LOW); //LED INICIA DESLIGADO
+  driver.init(); //INICIALIZA A COMUNICAÇÃO RF DO DRIVER
+  pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  digitalWrite(pinoLed, LOW); //LED INICIA DESLIGADO
 }
 void loop(){
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN]; //LÊ A MENSAGEM RECEBIDA (PALAVRA: led)
-    uint8_t buflen = sizeof(buf); //CRIA O COMPRIMENTO DO BUFFER PARA O TAMANHO DE buf
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN]; //LÊ A MENSAGEM RECEBIDA (PALAVRA: vibrar)
+  uint8_t buflen = sizeof(buf); //CRIA O COMPRIMENTO DO BUFFER PARA O TAMANHO DE buf
 
-    if(driver.recv(buf, &buflen)){ //SE O DRIVER RECEBEU buf(INTEIRO) E buflen (COMPRIMENTO DE DADOS), FAZ
-     str = ""; //VARIÁVEL RECEBE VAZIO
-     int i; //VARIÁVEL LOCAL DO TIPO INTEIRO
-       
-     for(int i = 0; i < buflen; i++){ //PARA i IGUAL A 0, ENQUANTO i MENOR QUE buflen, INCREMENTA i
-      str += (char)buf[i]; //VARIÁVEL RECEBE OS CARACTERES E FORMA A PALAVRA
-     }
-     if((str.equals("vibrar")) && (statusLed == 0)){ //SE str FOR IGUAL A "led" E statusLed FOR IGUAL A 0, FAZ
-      statusLed = 1; //VARIÁVEL RECEBE 1
-        digitalWrite(pinoLed, HIGH); //ACENDE O LED
-     }else{ //SENÃO, FAZ
-      if((str.equals("vibrar")) && (statusLed == 1)){ //SE str FOR IGUAL A "led" E statusLed FOR IGUAL A 1, FAZ
-          statusLed = 0; //VARIÁVEL RECEBE 0
-            digitalWrite(pinoLed, LOW); //APAGA O LED
-      }
-     }
+  if (driver.recv(buf, &buflen)) { //SE O DRIVER RECEBEU buf(INTEIRO) E buflen (COMPRIMENTO DE DADOS), FAZ
+    str = ""; //VARIÁVEL RECEBE VAZIO
+    int i; //VARIÁVEL LOCAL DO TIPO INTEIRO
+      
+    for (int i = 0; i < buflen; i++) { //PARA i IGUAL A 0, ENQUANTO i MENOR QUE buflen, INCREMENTA i
+    str += (char)buf[i]; //VARIÁVEL RECEBE OS CARACTERES E FORMA A PALAVRA
     }
+    
+    if (str.equals("vibrar")) { //SE str FOR IGUAL A "vibrar", FAZ
+      digitalWrite(pinoLed, HIGH); //ACENDE O LED
+      analogWrite(motorPin, 250);
+    }
+    else {
+      digitalWrite(pinoLed, LOW); //APAGA O LED
+      analogWrite(motorPin, 10);
+    }
+  }
 }
--- Comunicação-MódulosRF/Transmissor/Transmissor.ino
+++ Comunicação-MódulosRF/Transmissor/Transmissor.ino
@@ -1,21 +1,44 @@
 #include <RH_ASK.h> //INCLUSÃO DE BIBLIOTECA
 #include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+#include <Ultrasonic.h> // Leitura de distância com o sensor Grove Ultrasonic Ranger v2.0
+
+Ultrasonic ultrasonic(7);
+
+long distancia;
 
 RH_ASK driver; //CRIA O DRIVER PARA COMUNICAÇÃO
 
-const int pinoBotao = 2; //PINO DIGITAL UTILIZADO PELO PUSH BUTTON
-
 void setup(){
-    driver.init(); //INICIALIZA A COMUNICAÇÃO RF DO DRIVER
-    pinMode(pinoBotao, INPUT_PULLUP); //DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
-    //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
-}
-void loop(){
-    const char *msg = "vibrar"; //VARIÁVEL RECEBE O VALOR (led)
+  Serial.begin(9600); // Habilita a comunicação serial
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(motorPin, OUTPUT);
 
-    if(digitalRead(pinoBotao) == LOW){ //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
-        driver.send((uint8_t *)msg, strlen(msg)); //ENVIA AS INFORMAÇÕES PARA O RECEPTOR (PALAVRA: led)
-        driver.waitPacketSent(); //AGUARDA O ENVIO DAS INFORMAÇÕES
-        delay(200); //INTERVALO DE 200 MILISSEGUNDOS
-    }
+  driver.init(); //INICIALIZA A COMUNICAÇÃO RF DO DRIVER
+}
+
+void loop(){
+  distancia = ultrasonic.read();
+  Serial.print(distancia); // 0~400cm
+  Serial.println("cm");
+
+  const char *msg = "vibrar"; //VARIÁVEL RECEBE O VALOR (motor)
+
+  if (distancia < 100) {
+    driver.send((uint8_t *)msg, strlen(msg)); //ENVIA AS INFORMAÇÕES PARA O RECEPTOR (PALAVRA: vibrar)
+    driver.waitPacketSent(); //AGUARDA O ENVIO DAS INFORMAÇÕES
+
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+  }
+  else {
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+  }
+
+  delay(100);
 }
