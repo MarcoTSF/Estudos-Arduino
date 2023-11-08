3fe2ad2a605ccd3a44ae186f8a8ed6ecb47a4a32
Marco Tulio Salvador Filho <mtsf.salvador@gmail.com>
2023-11-07 22:40:04 -0300

    feat: created project #3 (Aula Modulo RF)

---
+++ Aula-MódulosRF/Receptor/Receptor.ino
@@ -0,0 +1,44 @@
+#include <RHReliableDatagram.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define TX_ADRESS 1
+#define RX_ADRESS 2
+
+uint8_t count = 0;
+uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+uint8_t tamanho;
+uint8_t from;
+
+RH_ASK driver;
+RHReliableDatagram gerente(driver, RX_ADRESS);
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!gerente.init()) {
+    Serial.println("Falha na inicialização");
+  }
+
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  if (gerente.available()) {
+    tamanho = sizeof(buf);
+
+    if(gerente.recvfromAck(buf, &tamanho, &from)) {
+      Serial.println("");
+      Serial.println("Recebido de: Marco e Lucas");
+      Serial.println("");
+      Serial.println("Mensagem:");
+      Serial.println((char*)buf);
+
+      if (strcmp((char*)buf, "Acionar LED") == 0) {
+        digitalWrite(8, HIGH);
+        delay(500);
+        digitalWrite(8, LOW);
+      }
+    } 
+  }
+}
